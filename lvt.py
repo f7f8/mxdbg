eb01b2fa2d8224a75d2adec6031698bbde793fa9
liwenwu <dashixiong.lee@gmail.com>
2018-05-01 15:27:06 +0800

    v5.2.15.4038 patches

--- lvt.py
+++ lvt.py
@@ -12,6 +12,7 @@ _IMAGE_NAME = 'MxBrowser-iPhone'
 
 _INS_RET = 0xd65f03c0
 _INS_NOP = 0xd503201f
+_INS_MOV_X0_1 = 0xd2800020
 
 def cmd_attach_remote(debugger, command, result, internal_dict):
     print("[%s] wait for remote target process: %s" % (__name__, _IMAGE_NAME))
@@ -29,6 +30,12 @@ def cmd_anti_anti_debug(debugger, command, result, internal_dict):
     patch_ptrace_entry(offset, proc)
     patch_isDebuggerPresent(offset, proc)
 
+    patch_police_isLegalEnvironmentAndUnLVTLimit(offset, proc)
+
+    patch_UeipSetDataDirectRequest(offset, proc)
+    patch_setupFlurryAnalytics(offset, proc)
+    patch_sendOnLineStatistics(offset, proc)
+
 
 def patch_ptrace_entry(base, proc):
     va = base + 0x10000c3fc
@@ -42,6 +49,32 @@ def patch_isDebuggerPresent(base, proc):
     va = base + 0x10045982c
     proc.WriteMemory(va, struct.pack('I', _INS_NOP), lldb.SBError())
     print '[%s] patch_isDebuggerPresent applied.' % __name__
+
+
+def patch_police_isLegalEnvironmentAndUnLVTLimit(base, proc):
+    va = base + 0x1004755e8
+    proc.WriteMemory(va, struct.pack('I', _INS_MOV_X0_1), lldb.SBError())
+    va = base + 0x1004755ec
+    proc.WriteMemory(va, struct.pack('I', _INS_RET), lldb.SBError())
+    print '[%s] patch_police_isLegalEnvironmentAndUnLVTLimit applied.' % __name__
+
+
+def patch_UeipSetDataDirectRequest(base, proc):
+    va = base + 0x100545650
+    proc.WriteMemory(va, struct.pack('I', _INS_RET), lldb.SBError())
+    print '[%s] patch_UeipSetDataDirectRequest applied.' % __name__
+
+
+def patch_setupFlurryAnalytics(base, proc):
+    va = base + 0x1000ce664
+    proc.WriteMemory(va, struct.pack('I', _INS_RET), lldb.SBError())
+    print '[%s] patch_setupFlurryAnalytics applied.' % __name__
+
+
+def patch_sendOnLineStatistics(base, proc):
+    va = base + 0x1000cde90
+    proc.WriteMemory(va, struct.pack('I', _INS_RET), lldb.SBError())
+    print '[%s] patch_sendOnLineStatistics applied.' % __name__
 
 
 def installCommands(debugger):
